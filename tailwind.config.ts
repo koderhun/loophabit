a83122a8a1823e488f1310a72e4c956e44b372bb
Ramil <koderhanov@gmail.com>
2025-02-05 12:09:43 +0300

    okkk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     flowbite.content()
   ],
   plugins: [
